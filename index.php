f4d5ccfc3c564591d8151f4d2f2e3ff7f9a8f29a
vagrant <mdingemanse@pcextreme.nl>
2017-08-08 10:59:34 +0000

    fixed headers

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
 use PDO;
-use RuntimeException;
 use Exception;
 
-$host   = "sql8.pcextreme.nl";
-$dbname = "62398vhost";
-$user   = "62398vhost";
-$pass   = "vhost";
+$host   = "";
+$dbname = "";
+$user   = "";
+$pass   = "";
 $query  = "SELECT 'something sweet'";
 
 // dsn is generated based on given db and host
@@ -19,9 +18,11 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (Exception $e) {
-    new RuntimeException('something went wrong while instantiating a PDO instance', 0);
+    http_response_code(503);
+    exit('error connection to the database');
 }
 
 $result = $pdo->query($query);
 
-die(json_encode($result));
+header('Content-type: application/json');
+echo json_encode($result);
