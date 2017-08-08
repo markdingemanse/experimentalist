39c63a0636baf7e184e8b073614da7806460991a
vagrant <mdingemanse@pcextreme.nl>
2017-08-08 10:10:09 +0000

    fixed some bugs

--- index.php
+++ index.php
@@ -2,51 +2,26 @@
 
 use PDO;
 use RuntimeException;
+use Exception;
 
-$host   = "";
-$dbname = "";
-$user   = "";
-$pass   = "";
-$query  = "";
+$host   = "sql8.pcextreme.nl";
+$dbname = "62398vhost";
+$user   = "62398vhost";
+$pass   = "vhost";
+$query  = "SELECT 'something sweet'";
 
 // dsn is generated based on given db and host
 $dsn    = "mysql:dbname=$dbname;host=$host";
-
 
 try {
     $pdo = new PDO($dsn, $user, $pass);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-    return $pdo;
 } catch (Exception $e) {
-    new RuntimeException('something went wrong while instantiating a PDO instance please check the experimentalist Log for more information', 0);
+    new RuntimeException('something went wrong while instantiating a PDO instance', 0);
 }
 
-$query = $query;
-$result = $this->db->query($query);
+$result = $pdo->query($query);
 
-$output = "<table>";
-foreach ($data as $key => $var) {
-    if ($key===0) {
-        $output .= '<tr>';
-        foreach ($var as $col => $val) {
-            $output .= "<td>" . $col . '</td>';
-        }
-        $output .= '</tr>';
-        foreach ($var as $col => $val) {
-            $output .= '<td>' . $val . '</td>';
-        }
-        $output .= '</tr>';
-    } else {
-        $output .= '<tr>';
-        foreach ($var as $col => $val) {
-            $output .= '<td>' . $val . '</td>';
-        }
-        $output .= '</tr>';
-    }
-}
-
-$output .= '</table>';
-echo $output;
+die(json_encode($result));
